0d73e0b07c31865e4ff9cc4c4b754aa8d0ad921d
Luke <lsh-0@users.noreply.github.com>
2023-01-06 17:41:51 +1030

    ppp, demonstration that things are working ...

--- ppp.go
+++ ppp.go
@@ -4,17 +4,16 @@ import (
 
 	// "github.com/lsh-0/ppp-go/internal/components/lax-proxy/interface"
 	// "github.com/lsh-0/ppp-go/internal/http"
+	"github.com/lsh-0/ppp-go/internal/api"
 	"github.com/lsh-0/ppp-go/internal/log"
 	// "github.com/lsh-0/ppp-go/internal/types"
 	// "github.com/lsh-0/ppp-go/internal/utils"
-	"fmt"
+	// "fmt"
 )
 
 func main() {
-	fmt.Println("started...")
-	log.Debug("debug?")
-	log.Info("info.")
-	log.Warn("*warn*")
-	log.Error("error!")
-	fmt.Println("...done.")
+	log.Debug("started")
+	opts := api.RequestConfig{}
+	log.Info("got: ", api.Request("/articles", opts))
+	log.Debug("done")
 }
